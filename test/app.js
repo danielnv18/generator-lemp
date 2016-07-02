bbc5b0c2c6e2892459826d0d4f3bea29b89355a6
Daniel Noyola <danielnv18@gmail.com>
2016-07-02 17:39:18 -0600

    TEST ALL THE THINGS

--- test/app.js
+++ test/app.js
@@ -27,6 +27,52 @@ describe('generator-lemp:app', function () {
     });
   });
 
+  describe('LEMP enviroment for drupal using php 5.6', function () {
+    before(function () {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          phpVersion: '5.6',
+          projectType: 'drupal'
+        })
+        .toPromise();
+    });
+
+    it('creates files', function () {
+      assert.file([
+        'docker-compose.yml',
+        'nginx/Dockerfile',
+        'nginx/nginx.conf',
+        'nginx/drupal.conf',
+        'php/Dockerfile',
+        'php/drupal.pool.conf',
+        'php/php.ini'
+      ]);
+    });
+  });
+
+  describe('LEMP enviroment for drupal using php 5.5', function () {
+    before(function () {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          phpVersion: '5.5',
+          projectType: 'drupal'
+        })
+        .toPromise();
+    });
+
+    it('creates files', function () {
+      assert.file([
+        'docker-compose.yml',
+        'nginx/Dockerfile',
+        'nginx/nginx.conf',
+        'nginx/drupal.conf',
+        'php/Dockerfile',
+        'php/drupal.pool.conf',
+        'php/php.ini'
+      ]);
+    });
+  });
+
   describe('LEMP enviroment for symfony using php 7', function () {
     before(function () {
       return helpers.run(path.join(__dirname, '../generators/app'))
@@ -49,4 +95,51 @@ describe('generator-lemp:app', function () {
       ]);
     });
   });
+
+  describe('LEMP enviroment for symfony using php 5.6', function () {
+    before(function () {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          phpVersion: '5.6',
+          projectType: 'symfony'
+        })
+        .toPromise();
+    });
+
+    it('creates files', function () {
+      assert.file([
+        'docker-compose.yml',
+        'nginx/Dockerfile',
+        'nginx/nginx.conf',
+        'nginx/symfony.conf',
+        'php/Dockerfile',
+        'php/symfony.pool.conf',
+        'php/php.ini'
+      ]);
+    });
+  });
+
+  describe('LEMP enviroment for symfony using php 5.5', function () {
+    before(function () {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          phpVersion: '5.5',
+          projectType: 'symfony'
+        })
+        .toPromise();
+    });
+
+    it('creates files', function () {
+      assert.file([
+        'docker-compose.yml',
+        'nginx/Dockerfile',
+        'nginx/nginx.conf',
+        'nginx/symfony.conf',
+        'php/Dockerfile',
+        'php/symfony.pool.conf',
+        'php/php.ini'
+      ]);
+    });
+  });
+
 });
