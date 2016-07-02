a005076300c965413ad3d2ba1d802e8402673175
Daniel Noyola <danielnv18@gmail.com>
2016-07-02 16:25:49 -0600

    More test.

--- test/app.js
+++ test/app.js
@@ -4,24 +4,49 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-lemp:app', function () {
-  before(function () {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({
-        phpVersion: '7.0',
-        projectType: 'drupal'
-      })
-      .toPromise();
+  describe('LEMP enviroment for drupal using php 7', function () {
+    before(function () {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          phpVersion: '7.0',
+          projectType: 'drupal'
+        })
+        .toPromise();
+    });
+
+    it('creates files', function () {
+      assert.file([
+        'docker-compose.yml',
+        'nginx/Dockerfile',
+        'nginx/nginx.conf',
+        'nginx/drupal.conf',
+        'php/Dockerfile',
+        'php/drupal.pool.conf',
+        'php/php.ini'
+      ]);
+    });
   });
 
-  it('creates files', function () {
-    assert.file([
-      'docker-compose.yml',
-      'nginx/Dockerfile',
-      'nginx/nginx.conf',
-      'nginx/drupal.conf',
-      'php/Dockerfile',
-      'php/drupal.pool.conf',
-      'php/php.ini'
-    ]);
+  describe('LEMP enviroment for symfony using php 7', function () {
+    before(function () {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          phpVersion: '7.0',
+          projectType: 'symfony'
+        })
+        .toPromise();
+    });
+
+    it('creates files', function () {
+      assert.file([
+        'docker-compose.yml',
+        'nginx/Dockerfile',
+        'nginx/nginx.conf',
+        'nginx/symfony.conf',
+        'php/Dockerfile',
+        'php/symfony.pool.conf',
+        'php/php.ini'
+      ]);
+    });
   });
 });
